cb046c6d7ecc8e5db030939c49a531b54a16cdfd
Vinuja M <101731879+vinuja24@users.noreply.github.com>
2024-07-09 19:00:28 +0530

    Delete Angularjs/OnlineShop/src/app directory

--- Angularjs/OnlineShop/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { WomenComponent } from './women/women.component';
-import { CartComponent } from './cart/cart.component';
-export const routes: Routes = [
-    { path: '/', component: HomeComponent },
-    { path: '/women', component: WomenComponent },
-    {path:'/cart',component:CartComponent}
-];
